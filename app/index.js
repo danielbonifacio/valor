0d09c704c1c6793704032250e3e0a5db8ec6c0cf
Daniel Bonifacio <danielbonfacio@outlook.com>
2019-01-14 11:36:21 -0200

    suporte aos alias de modulos

--- app/index.js
+++ app/index.js
@@ -1,18 +1,40 @@
+/**
+ * hpnode
+ * @author: Daniel Bonifacio <danielbonifacio@outlook.com>
+ */
 'use strict'
 
-global.Controller = require('../core/base/classes/controller.class')
+/**
+ * Registra os alias para importação
+ * Alias são meio que namespaces
+ * 
+ * Isso tem que vir antes de qualquer require!
+ */
+require('module-alias/register')
 
+
+// Globals
+global.Controller = require('Core/base/classes/controller.class')
+
+
+// Módulos
 const express = require('express')
 const bodyParser = require('body-parser')
-const registerControllers = require('../core/helpers/registerControllers')
+const registerControllers = require('Helpers/registerControllers')
 
+// Instância da aplicação
 const app = express()
 
+// Plugins
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
+// Registra todos os controllers da aplicação
 registerControllers(app)
 
+
+// Sobe o servidor na porta 3000
+// TODO: parametrizar a porta e normalizar
 app.listen(3000, (err) => {
     if (err) {
         console.log(err)
@@ -20,4 +42,3 @@ app.listen(3000, (err) => {
     }
     console.log('App running on http://localhost:3000')
 })
-

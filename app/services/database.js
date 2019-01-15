ab2dab26c6030c0c48b3ffb9bff9a92369efd68e
danielbonifacio <danielbonifacio@outlook.com>
2019-01-15 09:18:31 -0200

    fix deprecation warning

--- app/services/database.js
+++ app/services/database.js
@@ -1,9 +1,11 @@
 const sql = require('sequelize')
 const con = require('Config')
+const Op = sql.Op;
 
 const { db } = con
 
 module.exports = new sql(db.name, db.user, db.pass, {
     host: db.host,
     dialect: db.driver,
+    operatorsAliases: Op,
 })
